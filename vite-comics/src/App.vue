57703da509324e00105de0747c9356b23c459729
Morella92 <morena.piem@outlook.com>
2023-03-01 16:19:04 +0100

    FEAT components

--- vite-comics/src/App.vue
+++ vite-comics/src/App.vue
@@ -1,13 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppHeader from './components/AppHeader.vue'
+import AppMain from './components/AppMain.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  components: {
+    AppFooter,
+    AppHeader,
+    AppMain,
+  }
+}
 </script>
 
 <template>
-  <div>
-   
+  <div class="container"> 
+    ciao
   </div>
   
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
 </template>
 
 <style lang="scss" scoped>
+@use './style/general.scss';
+@use './style/partials/reset';
 </style>
---
+++ vite-comics/src/components/AppFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        ciao
+    </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
